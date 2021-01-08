8492d48bc689bb4ae1b6ca0e464de8f648e0d271
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2021-01-08 22:02:43 +0800

    More examples

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -16,8 +16,8 @@ template <class... Args>
 tpl(Args&&...) -> tpl<Args...>;
 
 int main() {
-  constexpr auto print = [](auto&& v) {
-    std::cout << "value contained: " << std::forward<decltype(v)>(v) << "\n";
+  constexpr auto print = [](const auto& v) {
+    std::cout << "value contained: " << v << "\n";
   };
 
   std::tuple t{42, 'c', "some sentence"};
@@ -28,17 +28,32 @@ int main() {
   std::optional<int> i1{42};
   std::optional<int> i2;
 
+  std::cout << "lvalue references\n=========\n";
   std::cout << "tuple:\n";
   dpsg::traverse(t, print);
   std::cout << "pair:\n";
   dpsg::traverse(p, print);
   std::cout << "variant (string):\n";
   dpsg::traverse(v, print);
-  std::cout << "optional (with value):\n";
-  dpsg::traverse(i1, print);
   std::cout << "optional(empty):\n";
   dpsg::traverse(i2, print);
+  std::cout << "optional (with value):\n";
+  dpsg::traverse(i1, print);
+  std::cout << std::endl;
 
+  std::cout << "rvalue references\n=========\n";
+  std::cout << "tuple:\n";
+  dpsg::traverse(std::tuple{0, "const char*", 'c', false}, print);
+  std::cout << "pair:\n";
+  dpsg::traverse(std::pair{12, 'd'}, print);
+  std::cout << "variant (string):\n";
+  dpsg::traverse(std::variant<std::string, int>(std::in_place_type<std::string>,
+                                                "string again"),
+                 print);
+  std::cout << "optional(empty):\n";
+  dpsg::traverse(std::optional<int>{}, print);
+  std::cout << "optional (with value):\n";
+  dpsg::traverse(std::optional<char>{'k'}, print);
   //   tpl t1{43, 'd', "something else"};
 
   //   dpsg::traverse(t1, print);
